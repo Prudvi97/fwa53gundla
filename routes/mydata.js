79118124a805d28084670e1717ea44632b6084ab
Prudvi97 <89538003+Prudvi97@users.noreply.github.com>
2021-10-27 23:40:21 -0500

    committed

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,12 +1,18 @@
+
 var express = require('express');
+
 var router = express.Router();
 
+
+
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('mydata', { 
-    title: 'Prudvi Reddy Gundla',
-    fact: 'I love learning French language by watching French series.'
-  });
+
+  res.render('mydata', { title: 'Prudvi Reddy Gundla' });
+
 });
+
+
 
 module.exports = router;
